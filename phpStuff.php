a5d4887e319bcf6db301e4b5bd0fc414b59de426
unknown <amartyn1996@gmail.com>
2017-10-14 22:01:14 -0400

    Some PHP stuff

---
+++ phpStuff.php
@@ -0,0 +1,35 @@
+<?php
+
+$servername = "localhost";
+$username = "fenwickgg";
+$password = "";
+
+$conn = mysql_connect($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+} 
+
+mysql_select_db("fenwickgg", $conn);
+
+$q1 = "select Name, Email, FacebookUsername, PhoneNum, DateFoodReceived from person where DateFoodReceived < (CURDATE() - INTERVAL 30 DAY);";
+$q2 = "select Name, County, Address, ReceiveFoodStamp from person";
+
+$queries = array(
+    1 => $q1,$q2
+);
+
+foreach($queries as $query) {
+    $result = mysql_query($query,$conn);
+
+    while ($row = mysql_fetch_assoc($result)) {
+        foreach($row as $key => $value) {
+            echo "$key = $value <br />";
+        }
+        echo "<br/>";
+    }
+    echo "---------------------<br/>";
+}
+mysql_close($conn);
+
+?>
