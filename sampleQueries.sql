3181042b6cc2b9d9f29b235194369c45021f2f17
unknown <amartyn1996@gmail.com>
2017-10-15 04:59:12 -0400

    Added monthly income to the inserts. don't know why it was letting me run it befre without it

--- sampleQueries.sql
+++ sampleQueries.sql
@@ -1,14 +1,14 @@
 insert into person 
-    (Email,FacebookUsername,PhoneNum,Name,Address,County,NumInHousehold,ReceiveFoodStamp)
-    values ("realJB@gmail.com","normalDude360",1236547890,"Joe Blow","123 Sesame St.", "Avery",5,1);
+    (Email,FacebookUsername,PhoneNum,Name,Address,County,NumInHousehold,ReceiveFoodStamp,MonthlyGrossIncome)
+    values ("realJB@gmail.com","normalDude360",1236547890,"Joe Blow","123 Sesame St.", "Avery",5,1,2500);
 
 insert into person 
-    (Email,FacebookUsername,PhoneNum,Name,Address,County,NumInHousehold,ReceiveFoodStamp)
-    values ("littleLass@gmail.com","lassieA9416",9271834752,"Lassie Awesome","911 NASCAR Dr.", "Sullivan",3,1);
+    (Email,FacebookUsername,PhoneNum,Name,Address,County,NumInHousehold,ReceiveFoodStamp,MonthlyGrossIncome)
+    values ("littleLass@gmail.com","lassieA9416",9271834752,"Lassie Awesome","911 NASCAR Dr.", "Sullivan",3,1,3000);
 
 insert into person 
-    (Email,FacebookUsername,PhoneNum,Name,Address,County,NumInHousehold,ReceiveFoodStamp)
-    values ("cakeEater@gmail.com","pastryLover3",2349991037,"Hungry Howie","Drury Ln.", "Boone",2,0);
+    (Email,FacebookUsername,PhoneNum,Name,Address,County,NumInHousehold,ReceiveFoodStamp,MonthlyGrossIncome)
+    values ("cakeEater@gmail.com","pastryLover3",2349991037,"Hungry Howie","Drury Ln.", "Boone",2,0,4000);
 
 insert into provider
     (ProviderName, FedFunded)
