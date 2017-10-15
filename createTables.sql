ab1c428ca0f4f94fb60a70025f70773cfc4354b1
unknown <amartyn1996@gmail.com>
2017-10-15 03:09:48 -0400

    changed max length of email from 25 characters to 30

--- createTables.sql
+++ createTables.sql
@@ -3,7 +3,7 @@ CREATE TABLE person (
 	Address varchar(25) NOT NULL,
 	County varchar(25) NOT NULL,
 	FacebookUsername varchar(25),
-	Email varchar(25) NOT NULL,
+	Email varchar(30) NOT NULL,
 	PhoneNum bigint(10) NOT NULL,
 	MonthlyGrossIncome Integer NOT NULL,
 	NumInHousehold Integer NOT NULL,
